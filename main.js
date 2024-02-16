5d4c3c696c6e66ac4ae23d81372a3b5555a58663
SdQL <sadavid2007@gmail.com>
2024-02-16 16:41:53 -0500

    Adding the funnctionality of dragging

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let isAnimating = false
-let pullDeltaX = 0 // distancia que la card se está arrastrando
+let pullDeltaX = 0 // distancia que la card que se está arrastrando
 
 function startDrag(e) {
     if(isAnimating) return
 
     // get the first article element
-    const actulCard = event.target.closest('article')
+    const actualCard = e.target.closest('article')
 
     // get initial position of mouse or finger
     const startX = e.pageX ?? e.touches[0].pageX
@@ -16,15 +16,42 @@ function startDrag(e) {
 
     document.addEventListener('touchmove', onMove, {passive: true});
     document.addEventListener('touchend', onEnd, {passive: true});
-}
 
-function onMove(event) {
+    function onMove(event) {
+        // current position of mouse or finger
+        const currentX = event.pageX ?? event.touches[0].pageX
+        // distance between the initial and current position
+        pullDeltaX = currentX - startX
+        // no distance, no movement
+        if(pullDeltaX === 0) return
 
-}
+        // change the state to indicate that the card is being dragged
+        isAnimating = true;
 
-function onEnd(event) {
+        // calculate the rotation degree based on the distance
+        const deg = pullDeltaX / 15;
+
+        // apply the transform to the card
+        actualCard.style.transform = `translateX(${pullDeltaX}px) rotate(${deg}deg)`;
+        // change the cursor to grabbing
+        actualCard.style.cursor = 'grabbing';
+
+        console.log(pullDeltaX)
+    }
+    
+    function onEnd(event) {
+        // renove the event listeners
+        document.removeEventListener('mousemove', onMove);
+        document.removeEventListener('mouseup', onEnd);
+
+        document.removeEventListener('touchmove', onMove);
+        document.removeEventListener('touchend', onEnd);
+
+        
+    }
     
 }
+
 
 document.addEventListener('mousedown', startDrag)
 document.addEventListener('touchstart', startDrag, {passive: true})
